e4d18410a60ee7b14d0f70ef2f83811c2e4d00aa
maple <maple.qq.com>
2023-12-26 12:06:37 +0800

    feat: 工程初始化

---
+++ .commitlintrc.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: 
+ * @Author: maple wang
+ * @Date: 2023-12-26 11:57:34
+ * @LastEditors: maple wang
+ * @LastEditTime: 2023-12-26 11:58:11
+ */
+module.exports = {
+    extends: ["@commitlint/config-conventional"]
+}; 
